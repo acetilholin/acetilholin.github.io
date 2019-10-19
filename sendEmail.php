4cb9534297eddac30a23d61eb795bf044c5a7be4
Gregor Kosmina <kosmina.gregor@gmail.com>
2019-10-19 16:33:43 +0200

    calculation improvement

--- sendEmail.php
+++ sendEmail.php
@@ -1,31 +1,30 @@
 <?php
 
-if((isset($_POST['email']))&&(isset($_POST['fullname'])&&$_POST['message']!="")){
+if ((isset($_POST['email'])) && (isset($_POST['fullname']) && $_POST['message'] != "")) {
     $to = "matjaz@avtowelt.com,tadej@avtowelt.com";
     $subject = 'Novo sporočilo';
 
     $email = $_POST['email'];
-    $date = date("Y-m-d");
+    $date = date("d.m.Y");
     $time = date('H:i');
     $msg = $_POST['message'];
 
 
     $message = "Preko spletne strani www.avtowelt.com, vam je bilo poslano sporočilo.    
     <br><br>
-    <b>Dne:</b> $date.' / ura:'.$time<br>    
+    <b>Datum in čas:</b> $date " . " $time<br>    
     <b>Vsebina:</b> $msg";
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= "From: '" . $email . "'" . "\r\n";
 
-    mail($to,$subject, $message, $headers);
+    mail($to, $subject, $message, $headers);
 
     echo json_encode(array('status' => 'success'));
 } else {
     echo json_encode(array('status' => 'error'));
 }
-
 
 
 ?>
